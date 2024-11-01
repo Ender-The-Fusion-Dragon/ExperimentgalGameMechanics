3dc682a8d45aae87b1bd7310dd576b59314438c3
Ender-The-Fusion-Dragon <enderassassino@gmail.com>
2024-11-01 22:34:12 +0000

    Added in basic alien code so it will disapear when hit by bullet and emit particles

--- Assets/BulletBehaviour.cs
+++ Assets/BulletBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour{
 
     private Rigidbody2D rigidBody;
+    public GameObject destructionParticle;
 
     // Start is called before the first frame update
     void Start(){
@@ -18,8 +19,13 @@ public class BulletBehaviour : MonoBehaviour{
 
     private void OnCollisionEnter2D(Collision2D collision){
 
-        if(collision.gameObject.tag == "Boarder"){
-            Destroy(this.gameObject);
+        GameObject explosion;
+
+        if(collision.gameObject.tag == "Enemy"){
+            explosion = Instantiate(destructionParticle, this.transform.position, Quaternion.Euler(0f, 180f, 0f));
+            Destroy(collision.gameObject);
         }
+
+        Destroy(this.gameObject);
     }
 }
