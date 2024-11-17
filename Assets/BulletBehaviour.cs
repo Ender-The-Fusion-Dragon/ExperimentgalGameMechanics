b5eaf65efe565c0754aee5550d960e78468e4355
Ender-The-Fusion-Dragon <enderassassino@gmail.com>
2024-11-17 21:28:28 +0000

    Added in a score to the game that changes when enemies destroyed

--- Assets/BulletBehaviour.cs
+++ Assets/BulletBehaviour.cs
@@ -6,12 +6,14 @@ public class BulletBehaviour : MonoBehaviour{
 
     private Rigidbody2D rigidBody;
     public GameObject destructionParticle;
-
     public float removeParticle;
+    private GameStateManager gameStateManager;
+
 
     // Start is called before the first frame update
     void Start(){
         rigidBody = this.GetComponent<Rigidbody2D>();
+        gameStateManager = GameObject.Find("GameState").GetComponent<GameStateManager>();
     }
 
     // Update is called once per frame
@@ -26,6 +28,7 @@ public class BulletBehaviour : MonoBehaviour{
 
         if(collision.gameObject.tag == "Enemy"){
             explosion = Instantiate(destructionParticle, this.transform.position, Quaternion.Euler(0f, 180f, 0f));
+            gameStateManager.adjustScore(100);
             Destroy(collision.gameObject);
 
             Destroy(explosion.gameObject, 0.3F);
--- Assets/GameStateManager.cs
+++ Assets/GameStateManager.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class GameStateManager : MonoBehaviour{
 
+    private int score;
 
     // Start is called before the first frame update
-    void Start(){
-
-    }
+    void Start(){}
 
     // Update is called once per frame
-    void Update(){
-        
+    void Update(){}
+
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int s){
+        score = s;
+    }
+
+    public void adjustScore(int s){
+        score += s;
+        Debug.Log("Score is " + score);
     }
 }
